1030900f12a4e1909f443114c68762d9b6b4d79f
smith15973 <smith.15973@osu.edu>
2024-09-30 15:25:54 -0400

    added infinite scroll

--- GroceryApp/pages/menu/Menu.jsx
+++ GroceryApp/pages/menu/Menu.jsx
@@ -1,52 +1,82 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { MenuDay } from "./MenuDay";
 import AddMealsToList from "./AddMealsToList";
-import { View, Button } from "react-native";
+import { View, Button, ScrollView } from "react-native";
+import { FlatList } from "react-native-bidirectional-infinite-scroll";
 import { useNavigation } from "@react-navigation/native";
+import { Dimensions } from "react-native";
 
 export function Menu() {
+    const ITEM_HEIGHT = 110;
+    const WINDOW_HEIGHT = Dimensions.get('window').height;
+    const LOAD_SCREEN_OF_ITEMS = Math.ceil(WINDOW_HEIGHT / ITEM_HEIGHT) + 1;
     const navigation = useNavigation();
     const [menus, setMenus] = useState([]);
 
 
     const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date(new Date().setDate(new Date().getDate() + 20)));
-    
-    
+    const [endDate, setEndDate] = useState(new Date());
 
-    function generateDateRange() {
-        
+
+
+    function generateDaysRange(startDate, endDate) {
+
         const days = [];
         let currentDate = new Date(startDate);
-        
+
         while (currentDate <= endDate) {
             days.push(new Date(currentDate));
             currentDate.setDate(currentDate.getDate() + 1);
         }
         return days;
     }
-    
 
 
 
-    async function loadMenus() {
-        const response = await axios.get('http://localhost:3000/menus', 
-        {
-            params: {
-                startDate: startDate,
-                endDate: endDate,
+
+    async function loadMenusInRange(startDate, endDate) {
+        const response = await axios.get('http://localhost:3000/menus',
+            {
+                params: {
+                    startDate: startDate,
+                    endDate: endDate,
+                }
             }
-        }
         );
         const menuData = response.data;
-        const allDates = generateDateRange();
-        setMenus(response.data);
+        const allDays = generateDaysRange(startDate, endDate);
+
+        const mergedData = allDays.map(day => {
+            const menuDay = menuData.find(menuDay => new Date(menuDay.date).toDateString() === day.toDateString());
+            return menuDay || { date: day, meals: [] };
+        })
+
+        setMenus(mergedData);
+    }
+
+    async function loadOlderMenuDays() {
+        const newStartDate = new Date(startDate);
+        newStartDate.setDate(startDate.getDate() - LOAD_SCREEN_OF_ITEMS);
+        setStartDate(newStartDate);
+        loadMenusInRange(newStartDate, endDate);
+    }
+
+    async function loadNewerMenuDays() {
+        const newEndDate = new Date(endDate);
+        newEndDate.setDate(endDate.getDate() + LOAD_SCREEN_OF_ITEMS);
+        setEndDate(newEndDate);
+        loadMenusInRange(startDate, newEndDate);
     }
 
     useEffect(() => {
-        loadMenus();
-        generateDateRange();
+        const oneDayBeforeStartDate = new Date(startDate);
+        oneDayBeforeStartDate.setDate(startDate.getDate() - 1);
+        setStartDate(oneDayBeforeStartDate);
+        const initialEndDate = new Date();
+        initialEndDate.setDate(startDate.getDate() + LOAD_SCREEN_OF_ITEMS);
+        setEndDate(initialEndDate);
+        loadMenusInRange(oneDayBeforeStartDate, initialEndDate);
     }, []);
 
 
@@ -96,26 +126,48 @@ export function Menu() {
         />
     });
 
+    
+
     return (
+        <FlatList
+            data={menus}
+            renderItem={({ item }) => (
+                <MenuDay menuDay={item} onMealSelect={handleMealSelect} onSelectDay={handleSelectDay} mealsSelected={mealsSelected} />
+            )}
+            onEndReached={loadNewerMenuDays}
+            onEndReachedThreshold={0.8}
 
-        <View className="menu">
-            {menus.map(menuDay => {
-                function mealSort(a, b) {
-                    const order = ['Breakfast', 'Lunch', 'Snack', 'Dinner', 'Dessert'];
-                    return order.indexOf(a.type) - order.indexOf(b.type);
-                }
-
-                // const meals = menuDay.meals.sort(mealSort);
-                return (
-
-                    <View key={menuDay._id}>
-                        <MenuDay menuDay={menuDay} onMealSelect={handleMealSelect} onSelectDay={handleSelectDay} mealsSelected={mealsSelected} />
-                    </View>
+            onStartReached={loadOlderMenuDays}
+            onStartReachedThreshold={0.3}
+            showDefaultLoadingIndicators={true}
+            getItemLayout={(data, index) => (
+                { length: 110, offset: 110 * index, index }
+            )}
+            keyExtractor={(item) => item.date}
+            initialScrollIndex={1}
 
 
-                );
-            })}
-        </View>
+
+        />
+
+        // <ScrollView ref={scrollViewRef} onScroll={handleScroll} scrollEventThrottle={16}>
+        //     {menus.map(menuDay => {
+        //         function mealSort(a, b) {
+        //             const order = ['Breakfast', 'Lunch', 'Snack', 'Dinner', 'Dessert'];
+        //             return order.indexOf(a.type) - order.indexOf(b.type);
+        //         }
+
+        //         // const meals = menuDay.meals.sort(mealSort);
+        //         return (
+
+        //             <View key={menuDay._id}>
+        //                 <MenuDay menuDay={menuDay} onMealSelect={handleMealSelect} onSelectDay={handleSelectDay} mealsSelected={mealsSelected} />
+        //             </View>
+
+
+        //         );
+        //     })}
+        // </ScrollView>
 
     );
 }
