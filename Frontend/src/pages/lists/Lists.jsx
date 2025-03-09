b39f9a549f1f2e2d067aa46a7a73310c465a7aa7
Noah Smith <smith.15973@osu.edu>
2025-03-09 17:18:18 -0400

    updated style of lists

--- Frontend/src/pages/lists/Lists.jsx
+++ Frontend/src/pages/lists/Lists.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
-import { Button, List, ListItem, ListItemText } from "@mui/material";
+import { Button, List, ListItem, ListItemText, IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import ListItemButton from '@mui/material/ListItemButton';
+
+import TrashIcon from '@mui/icons-material/Delete';
 
 export function Lists() {
 
@@ -21,34 +24,20 @@ export function Lists() {
         <>
             <h3>Lists</h3>
             <Button variant="outlined">Create New List</Button>
-            <List sx={{ width: '100%', maxWidth: '100%' }}>
+            <List sx={{ width: '100%', maxWidth: '100%', bgcolor: 'background.paper' }}>
                 {lists.map(list => {
                     return (
-                        // <ListItem
-                        //     key={list._id}
-                        //     secondaryAction={
-                        //         <IconButton edge="end" aria-label="delete" onClick={() => { handleDelete(list._id) }}>
-                        //             <TrashIcon />
-                        //         </IconButton>
-                        //     }
-                        //     disablePadding
-                        // >
-                        //     <ListItemButton role={undefined} dense>
-                        //         {ingredient.complete !== undefined ? <ListItemIcon>
-                        //             <Checkbox
-                        //                 edge="start"
-                        //                 checked={ingredient.complete}
-                        //                 tabIndex={-1}
-                        //                 disableRipple
-                        //                 inputProps={{ 'aria-labelledby': labelId }}
-                        //                 onClick={handleToggle(ingredient._id)}
-                        //             />
-                        //         </ListItemIcon> : ''}
-                        //         <ListItemText id={labelId} primary={`${ingredient.item.name} ${ingredient.quantity ? `--- ${ingredient.quantity} ${ingredient.unit}` : ''}`} />
-                        //     </ListItemButton>
-                        // </ListItem>
-                        <ListItem onClick={() => { navigate(`/Lists/${list._id}`) }} key={list._id}>
-                            <ListItemText href={`#/Lists/${list._id}`}>{list.name}</ListItemText>
+                        <ListItem key={list._id}
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="delete" onClick={() => { handleDelete(list._id) }}>
+                                    <TrashIcon />
+                                </IconButton>
+                            }
+                            disablePadding
+                        >
+                            <ListItemButton role={undefined} onClick={() => { navigate(`/lists/${list._id}`) }} dense>
+                                <ListItemText >{list.name}</ListItemText>
+                            </ListItemButton>
                         </ListItem>
                     )
                 })}
